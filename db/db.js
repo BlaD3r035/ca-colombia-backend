1b9368b34060755f6f2af1a68cf25b1b025a4b4d
BlaD3r <estepaestupinansamuelalejandro@gmail.com>
2025-03-18 20:39:55 -0500

    minor change

--- db/db.js
+++ db/db.js
@@ -12,23 +12,17 @@ const pool = mysql.createPool({
     waitForConnections: true,
 }).promise();
 
-function sendDiscordWebhook(message) {
-    axios.post(DISCORD_WEBHOOK_URL, { content: message })
-        .then(() => console.log('Mensaje enviado a Discord'))
-        .catch(err => console.error('Error al enviar mensaje a Discord:', err));
-}
+
 
 pool.getConnection()
     .then(connection => {
         const successMessage = `✅ Conectado a la base de datos MySQL con ID ${connection.threadId}`;
         console.log(successMessage);
-        sendDiscordWebhook(successMessage);
         connection.release();
     })
     .catch(err => {
         const errorMessage = `❌ Error al conectar a la base de datos: ${err.message}`;
         console.error(errorMessage);
-        sendDiscordWebhook(errorMessage);
     });
 
 module.exports = pool;
